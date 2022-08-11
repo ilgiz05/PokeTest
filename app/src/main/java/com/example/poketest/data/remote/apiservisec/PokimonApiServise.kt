fb9078dee31ad7d9ce7424319abe40a66f621d2c
toktonaliev1 <toktonalievamantur66@gmail.com>
2022-08-11 15:29:23 +0300

    изменил в api services

--- app/src/main/java/com/example/poketest/data/remote/apiservisec/PokimonApiServise.kt
+++ app/src/main/java/com/example/poketest/data/remote/apiservisec/PokimonApiServise.kt
@@ -1,10 +1,12 @@
 package com.example.poketest.data.remote.apiservisec
 
+import com.example.poketest.models.PokemonResponse
 import com.example.poketest.models.PokimonsModel
 import retrofit2.http.GET
 
 interface PokimonApiServise {
 
-    @GET("v2")
-    suspend fun fetchPokimons(): List<PokimonsModel>
+    @GET("pokemon")
+    suspend fun fetchPokimons(
+    ): PokemonResponse<PokimonsModel>?
 }
